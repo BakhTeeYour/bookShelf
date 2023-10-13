a2fc9bd51004c47a1490f5961dc495c86a65eaf1
BakhTeeYour <bakh.achelov@gmail.com>
2023-10-13 18:03:10 +0500

    feat: implement MaxResults

--- src/api/books.ts
+++ src/api/books.ts
@@ -4,7 +4,7 @@ import {apiRoutes} from "../consts/apiRoutes";
 const key = 'AIzaSyDnZd5JbaNW34J_ZZkGkq_bCbkAoY2cBn4'
 
 const getBooks = (q: string, orderBy: string, filter: string, startIndex: string) => axios.get(apiRoutes.getBooks(q, filter), {
-    params: {orderBy, key, startIndex}
+    params: {orderBy, key, startIndex, maxResults: '30'}
 });
 
 export const booksApi = {
